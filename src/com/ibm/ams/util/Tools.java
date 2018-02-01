743d76859e7c337bfa5eb22b7cdcd24d7814ed32
liangq123 <liangqiangayj@163.com>
2018-02-01 18:43:09 +0800

    提交公共方法

--- src/com/ibm/ams/util/Tools.java
+++ src/com/ibm/ams/util/Tools.java
@@ -249,9 +249,35 @@ public class Tools {
 		return "";
 	}
 	
+	/**
+	 * 需求1：定义功能，将{34,12,67}数组中的元素转成字符串  "[34,12,67]" 
+	 * @param arr
+	 * @return
+	 */
+	public static String toString(String[] arr)  
+    {  
+        //1,定义字符串变量。  
+        String temp = "[";  
+        //2,遍历数组。将每一个数组的元素和字符串相连接。  
+        if (arr!=null && arr.length==0) {
+			return "[]";
+		}
+        for(int x = 0; x < arr.length; x++)  
+        {  
+            //判断，不是最后一个元素，后面连接逗号，是最后一个元素，后面不连接逗号。  
+            if(x!=arr.length-1)  
+                temp = temp + arr[x] + ",";  
+            else  
+                temp = temp + arr[x] + "]";  
+        }  
+        //3，将连接后的字符串返回。哦耶！  
+        return temp;  
+    }  
 	
 	public static void main(String[] args) {
 		System.out.println(getRandomNum());
+		String [] a={"1"};
+		System.out.println(toString(a));
 	}
 	
 }
